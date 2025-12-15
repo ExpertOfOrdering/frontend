eb80ed42ab0844ba74b6d9844f6c3995d284a6f0
chootkuri <doyadoya@sookmyung.ac.kr>
2025-12-15 22:18:58 +0900

    Design(#9):회원가입 Input 스타일 적용

---
+++ src/components/shared/AuthInput.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function AuthInput({ label, type, placeholder }) {
+  return (
+    <>
+      <div className='flex flex-col gap-6 justify-center items-center'>
+        <label className='text-[3.125rem] font-semibold'>{label}</label>
+        <input
+          type={type}
+          placeholder={placeholder}
+          className='flex justify-center items-center w-196.5 h-25.5 placeholder-[#6E6E6E] text-center text-[2.25rem] font-medium'
+        />
+      </div>
+    </>
+  )
+}
+
+export default AuthInput
