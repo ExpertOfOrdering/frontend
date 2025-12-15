6d5db6b4c42292d9c06e538a29a25bdc0be5a86c
chootkuri <doyadoya@sookmyung.ac.kr>
2025-12-15 20:16:06 +0900

    Feat(#5):온보딩 UI 구조 구성

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import React from "react";
-import { Outlet } from "react-router-dom";
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+import Onboarding from './components/Onboarding/Onboarding'
 
 function App() {
   return (
     <>
       <Outlet />
+      <Onboarding />
     </>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Onboarding/Onboarding.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Logo from '@/assets/OnboardingLogo.svg'
+
+function Onboarding() {
+  return (
+    <>
+      <div className=''>
+        <img src={Logo} alt='Onboarding Image' className='w-full h-auto' />
+      </div>
+    </>
+  )
+}
+
+export default Onboarding
