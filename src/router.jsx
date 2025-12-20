2c6dc5148fb188780dc4a2e931d3446d4c9529bc
yexnsxo <yslove829@naver.com>
2025-12-20 23:04:58 +0900

    Feat(#11): 배경음 전역 적용

--- src/router.jsx
+++ src/router.jsx
@@ -1,15 +1,27 @@
 import { createBrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import Onboarding from '@/components/Onboarding/Onboarding.jsx'
+import PracticeLogPage from './pages/PracticeLogPage.jsx'
+import SettingPage from './pages/SettingPage.jsx'
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
-  },
-  {
-    path: '/onboarding',
-    element: <Onboarding />,
+    children: [
+      {
+        path: '/onboarding',
+        element: <Onboarding />,
+      },
+      {
+        path: '/practiceLog',
+        element: <PracticeLogPage />,
+      },
+      {
+        path: '/setting',
+        element: <SettingPage />,
+      },
+    ],
   },
 ])
 
