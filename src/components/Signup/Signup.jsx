63bf71a1e511f6f81dba570c7b783d97b36642b9
chootkuri <doyadoya@sookmyung.ac.kr>
2025-12-22 19:53:16 +0900

    Style(#9):회원가입 전체 화면 비율 조정

--- src/components/Signup/Signup.jsx
+++ src/components/Signup/Signup.jsx
@@ -5,13 +5,15 @@ import AuthButton from '@/components/shared/AuthButton.jsx'
 
 function Signup() {
   return (
-    <>
-      <AuthTitle title='계정 회원가입' />
-      <AuthInput label='사용자 이름' type='name' placeholder='이름을 입력해 주세요' />
-      <AuthInput label='계정의 아이디' type='id' placeholder='아이디를 입력해 주세요' />
-      <AuthInput label='계정의 비밀번호' type='password' placeholder='비밀번호를 입력해 주세요' />
-      <AuthButton text='회원가입하기' />
-    </>
+    <div className='min-h-screen w-full flex justify-center'>
+      <div className='w-full min-h-screen max-w-208.5 flex flex-col items-center justify-center gap-10'>
+        <AuthTitle title='계정 회원가입' />
+        <AuthInput label='사용자 이름' type='text' placeholder='이름을 입력해 주세요' />
+        <AuthInput label='계정의 아이디' type='text' placeholder='아이디를 입력해 주세요' />
+        <AuthInput label='계정의 비밀번호' type='password' placeholder='비밀번호를 입력해 주세요' />
+        <AuthButton text='회원가입하기' />
+      </div>
+    </div>
   )
 }
 
