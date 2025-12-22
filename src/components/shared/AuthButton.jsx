62595444d75b04ba84d189fa4c6d410efd645fa4
chootkuri <doyadoya@sookmyung.ac.kr>
2025-12-22 19:09:34 +0900

    Feat(#9):로그인/회원가입 버튼 공통 컴포넌트로 분리

---
+++ src/components/shared/AuthButton.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function AuthButton(text) {
+  return (
+    <>
+      <button
+        className='w-109.5 h-31.75 p-2.5 text-center color-[#FFF] bg-[#58AFFF] rounded-[1.25rem]'
+        value={text}
+      />
+    </>
+  )
+}
+
+export default AuthButton
