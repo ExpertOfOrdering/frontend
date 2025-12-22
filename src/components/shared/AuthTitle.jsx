86693dc6c333338df4d99dee999dcaeea74c0692
chootkuri <doyadoya@sookmyung.ac.kr>
2025-12-22 19:09:15 +0900

    Feat(#9):로그인/회원가입 제목 공통 컴포넌트로 분리

---
+++ src/components/shared/AuthTitle.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function AuthTitle(title) {
+  return (
+    <>
+      <div>
+        <span>주문의 달인</span>
+        <span>{title}</span>
+      </div>
+    </>
+  )
+}
+
+export default AuthTitle
