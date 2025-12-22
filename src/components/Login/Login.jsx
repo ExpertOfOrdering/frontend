4ea859c27a5d6a0689202feb6fb9f3ce60fba585
chootkuri <doyadoya@sookmyung.ac.kr>
2025-12-23 01:06:13 +0900

    Feat(#15):로그인하기 버튼 클릭 시 메인 페이지로 이동 처리

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -2,14 +2,21 @@ import React from 'react';
 import AuthTitle from '@/components/shared/AuthTitle.jsx';
 import AuthInput from '@/components/shared/AuthInput.jsx';
 import AuthButton from '@/components/shared/AuthButton.jsx';
+import { useNavigate } from 'react-router-dom';
 
 function Login() {
+    const navigate = useNavigate();
+
+    const handleLogin = () => {
+        navigate('/main');
+    }
+
     return (
         <>
             <AuthTitle title="계정 로그인하기" />
             <AuthInput label="계정의 아이디" placeholder="아이디를 입력해 주세요" value={} onChange={}/>
             <AuthInput label="계정의 비밀번호" placeholder="비밀번호를 입력해 주세요" value={} onChange={}/>
-            <AuthButton text="로그인하기" onClick={} />
+            <AuthButton text="로그인하기" onClick={handleLogin} />
             <span><u>회원가입하기</u></span>
         </>
     );
