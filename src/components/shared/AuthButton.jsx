7c4d8078cd2bba5783328b4074143bb2211f07d8
chootkuri <doyadoya@sookmyung.ac.kr>
2025-12-22 23:09:25 +0900

    Feat(#9):로그인/회원가입 버튼 클릭 이벤트 적용

--- src/components/shared/AuthButton.jsx
+++ src/components/shared/AuthButton.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 
-function AuthButton({ text }) {
+function AuthButton({ text, onClick }) {
   return (
     <>
-      <button className='w-109.5 h-31.75 p-2.5 text-center text-white bg-[#58AFFF] rounded-[1.25rem] text-[3.75rem] font-extrabold'>
+      <button
+        className='w-109.5 h-31.75 p-2.5 text-center text-white bg-[#58AFFF] rounded-[1.25rem] text-[3.75rem] font-extrabold'
+        onClick={onClick}
+      >
         {text}
       </button>
     </>
--- src/components/shared/AuthTitle.jsx
+++ src/components/shared/AuthTitle.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function AuthTitle({ title }) {
   return (
     <>
-      <div className='flex flex-col items-center justify-center gap-[0.81rem]'>
+      <div className='flex flex-col items-center justify-center'>
         <span className='text-[#58AFFF] font-[SBAggro] text-[3.87994rem]'> 주문의 달인</span>
         <span className='font-bold text-[4.375rem]'>{title}</span>
       </div>
