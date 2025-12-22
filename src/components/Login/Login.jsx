bc632c026c4a7e133a398af8a8b915288568a724
chootkuri <doyadoya@sookmyung.ac.kr>
2025-12-23 02:16:19 +0900

    Style(#15):인증 공통 레이아웃 높이 조정

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -18,28 +18,30 @@ function Login() {
   }
 
   return (
-    <AuthLayout title='계정 로그인하기'>
-      <AuthInput
-        label='계정의 아이디'
-        placeholder='아이디를 입력해 주세요'
-        type='text'
-        value={id}
-        onChange={(e) => setId(e.target.value)}
-      />
-      <AuthInput
-        label='계정의 비밀번호'
-        placeholder='비밀번호를 입력해 주세요'
-        type='password'
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <AuthButton text='로그인하기' onClick={handleLogin} disabled={!id || !password} />
-      <Link to='/signup'>
-        <span className='text-[#58AFFF] text-[3.125rem] font-semibold underline underline-offset-5'>
-          회원가입하기
-        </span>
-      </Link>
-    </AuthLayout>
+    <>
+      <AuthLayout title='계정 로그인하기'>
+        <AuthInput
+          label='계정의 아이디'
+          placeholder='아이디를 입력해 주세요'
+          type='text'
+          value={id}
+          onChange={(e) => setId(e.target.value)}
+        />
+        <AuthInput
+          label='계정의 비밀번호'
+          placeholder='비밀번호를 입력해 주세요'
+          type='password'
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <AuthButton text='로그인하기' onClick={handleLogin} disabled={!id || !password} />
+        <Link to='/signup'>
+          <span className='text-[#58AFFF] text-[3.125rem] font-semibold underline underline-offset-5'>
+            회원가입하기
+          </span>
+        </Link>
+      </AuthLayout>
+    </>
   )
 }
 
--- src/components/shared/Auth/AuthLayout.jsx
+++ src/components/shared/Auth/AuthLayout.jsx
@@ -3,12 +3,14 @@ import AuthTitle from '@/components/shared/Auth/AuthTitle.jsx'
 
 function AuthLayout({ title, children }) {
   return (
-    <div className='min-h-screen w-full grid grid-rows-[auto_1fr_auto]'>
-      <div className='flex justify-center pt-20'>
-        <AuthTitle title={title} />
-      </div>
-      <div className='flex justify-center items-center'>
-        <div className='w-full max-w-208.5 flex flex-col items-center gap-10'>{children}</div>
+    <div className='w-full flex justify-center'>
+      <div className='w-full max-w-208.5 h-298.5 grid grid-rows-[auto_1fr] pt-20'>
+        <div className='flex justify-center'>
+          <AuthTitle title={title} />
+        </div>
+        <div className='flex items-center justify-center'>
+          <div className='flex flex-col items-center gap-10'>{children}</div>
+        </div>
       </div>
     </div>
   )
