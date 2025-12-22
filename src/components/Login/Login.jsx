d8223ab0483fe4ced997e3750432a1ac167549f2
chootkuri <doyadoya@sookmyung.ac.kr>
2025-12-23 01:13:02 +0900

    Feat(#15):로그인 입력값 상태 관리

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,25 +1,44 @@
-import React from 'react';
-import AuthTitle from '@/components/shared/AuthTitle.jsx';
-import AuthInput from '@/components/shared/AuthInput.jsx';
-import AuthButton from '@/components/shared/AuthButton.jsx';
-import { useNavigate } from 'react-router-dom';
+import React from 'react'
+import AuthTitle from '@/components/shared/AuthTitle.jsx'
+import AuthInput from '@/components/shared/AuthInput.jsx'
+import AuthButton from '@/components/shared/AuthButton.jsx'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 function Login() {
-    const navigate = useNavigate();
+  const [id, setId] = useState('')
+  const [password, setPassword] = useState('')
 
-    const handleLogin = () => {
-        navigate('/main');
-    }
+  const navigate = useNavigate()
 
-    return (
-        <>
-            <AuthTitle title="계정 로그인하기" />
-            <AuthInput label="계정의 아이디" placeholder="아이디를 입력해 주세요" value={} onChange={}/>
-            <AuthInput label="계정의 비밀번호" placeholder="비밀번호를 입력해 주세요" value={} onChange={}/>
-            <AuthButton text="로그인하기" onClick={handleLogin} />
-            <span><u>회원가입하기</u></span>
-        </>
-    );
+  const handleLogin = () => {
+    navigate('/main')
+  }
+
+  return (
+    <>
+      <AuthTitle title='계정 로그인하기' />
+      <AuthInput
+        label='계정의 아이디'
+        placeholder='아이디를 입력해 주세요'
+        type='text'
+        value={id}
+        onChange={(e) => setId(e.target.value)}
+      />
+      <AuthInput
+        label='계정의 비밀번호'
+        placeholder='비밀번호를 입력해 주세요'
+        type='password'
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <AuthButton text='로그인하기' onClick={handleLogin} />
+      <Link to='/signup'>
+        <span className='underline cursor-pointer'>회원가입하기</span>
+      </Link>
+    </>
+  )
 }
 
-export default Login;
+export default Login
