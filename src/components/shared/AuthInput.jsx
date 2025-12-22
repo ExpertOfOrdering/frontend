2187bb5e596dd0815c23346ac8c237de4a55c892
chootkuri <doyadoya@sookmyung.ac.kr>
2025-12-22 19:54:34 +0900

    Style(#9):인증 페이지 input css 적용

--- src/components/shared/AuthInput.jsx
+++ src/components/shared/AuthInput.jsx
@@ -8,7 +8,7 @@ function AuthInput({ label, type, placeholder }) {
         <input
           type={type}
           placeholder={placeholder}
-          className='flex justify-center items-center w-196.5 h-25.5 placeholder-[#6E6E6E] text-center text-[2.25rem] font-medium'
+          className='flex justify-center items-center w-196.5 h-25.5 placeholder-[#6E6E6E] text-center text-[2.25rem] font-medium bg-[#EAEAEA] rounded-[0.9375rem]'
         />
       </div>
     </>
