80adde2c025b0f1caccd3384f560d3fde458b78e
chootkuri <doyadoya@sookmyung.ac.kr>
2025-12-22 19:54:01 +0900

    Style(#9):인증 페이지 제목 css 적용

--- src/components/shared/AuthTitle.jsx
+++ src/components/shared/AuthTitle.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-function AuthTitle(title) {
+function AuthTitle({ title }) {
   return (
     <>
-      <div>
-        <span>주문의 달인</span>
-        <span>{title}</span>
+      <div className='flex flex-col items-center justify-center gap-[0.81rem]'>
+        <span className='text-[#58AFFF] font-[SBAggro] text-[3.87994rem]'> 주문의 달인</span>
+        <span className='font-bold text-[4.375rem]'>{title}</span>
       </div>
     </>
   )
