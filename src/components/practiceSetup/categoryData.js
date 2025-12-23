9950ca4ecdbf1f431d32d484371eeca7614c2b4a
chootkuri <doyadoya@sookmyung.ac.kr>
2025-12-23 18:10:46 +0900

    Feat(#20):연습 난이도 & 카테고리 데이터 파일 추가

---
+++ src/components/practiceSetup/categoryData.js
@@ -0,0 +1,35 @@
+import cafe from '@/assets/Cafe.png'
+import cafeteria from '@/assets/Cafeteria.png'
+import fastFood from '@/assets/FastFood.png'
+import convStore from '@/assets/ConvenienceStore.png'
+
+export const categoryData = [
+  {
+    id: 1,
+    label: '카페/음료',
+    bgColor: 'bg-[#E6FFE4]',
+    textColor: 'text-[#054900]',
+    img: cafe,
+  },
+  {
+    id: 2,
+    label: '식당/요리',
+    bgColor: 'bg-[#E4F2FF]',
+    textColor: 'text-[#001569]',
+    img: cafeteria,
+  },
+  {
+    id: 3,
+    label: '패스트푸드',
+    bgColor: 'bg-[#FFF2E4]',
+    textColor: 'text-[#491700]',
+    img: fastFood,
+  },
+  {
+    id: 4,
+    label: '무인 편의점',
+    bgColor: 'bg-[#EDE4FF]',
+    textColor: 'text-[#330049]',
+    img: convStore,
+  },
+]
---
+++ src/components/practiceSetup/levelData.js
@@ -0,0 +1,23 @@
+export const levelData = [
+  {
+    id: 1,
+    label: '초보',
+    bgColor: 'bg-[#FFEA96]',
+    textColor: 'text-[#5A3A00]',
+    isRecommended: false,
+  },
+  {
+    id: 2,
+    label: '중수',
+    bgColor: 'bg-[#96FFAE]',
+    textColor: 'text-[#065A00]',
+    isRecommended: false,
+  },
+  {
+    id: 3,
+    label: '달인',
+    bgColor: 'bg-[#96E6FE]',
+    textColor: 'text-[#00275A]',
+    isRecommended: false,
+  },
+]
