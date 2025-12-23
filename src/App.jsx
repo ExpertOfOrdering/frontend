b626b49c111a3e46b0421b06aad785558e59fdad
yexnsxo <yslove829@naver.com>
2025-12-23 16:58:32 +0900

    Feat(#11): 효과음 전역 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from 'react-router-dom'
 import BgmManager from './components/setting/BgmManager'
+import GlobalClickSfxManager from './components/setting/GlobalClickSfxManager'
 
 function App() {
   return (
     <>
       <BgmManager />
+      <GlobalClickSfxManager />
       <Outlet />
     </>
   )
