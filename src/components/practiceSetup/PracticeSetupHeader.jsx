962bcd51579a201e6db8cb62af2833e89baca86a
chootkuri <doyadoya@sookmyung.ac.kr>
2025-12-24 00:18:15 +0900

    Feat(#20):버튼 클릭 이벤트 추가 및 단계별 화면 전환 기능 추가

--- src/components/practiceSetup/PracticeSetupHeader.jsx
+++ src/components/practiceSetup/PracticeSetupHeader.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 
-function PracticeSetupHeader() {
+function PracticeSetupHeader({ onBack }) {
   return (
-    <div className='flex items-end w-full h-44 border-b-[#919191] border-b-2'>
-      <button className='mb-7 ml-[2.56rem] mr-[12.06rem] w-25.25 h-16.5 bg-[#B7B7B7] rounded-[0.625rem] text-[2.8125rem] text-white font-semibold'>
+    <div className='flex items-end w-full h-30 border-b-[#919191] border-b-2'>
+      <button
+        className='mb-7 ml-[2.56rem] mr-[12.06rem] w-25.25 h-16.5 bg-[#B7B7B7] rounded-[0.625rem] text-[2.8125rem] text-white font-semibold cursor-pointer hover:opacity-85'
+        onClick={onBack}
+      >
         이전
       </button>
       <span className='mb-7 text-[2.8125rem] font-semibold'>연습 준비</span>
