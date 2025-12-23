66559fff0b648c2182fdce11bd6cea6a081e0d26
chootkuri <doyadoya@sookmyung.ac.kr>
2025-12-24 07:33:48 +0900

    Style(#28):연습하기 하단바 css 적용

--- src/components/practice/practiceHeader.jsx
+++ src/components/practice/practiceHeader.jsx
@@ -1,17 +1,50 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
-function practiceHeader({ misson }) {
+function PracticeHeader({ misson }) {
+  const navigate = useNavigate()
   return (
-    <div>
-      <div>
-        <span>연습 중</span>
-        <button>종료</button>
+    <div className='flex flex-col items-center'>
+      <div
+        className='
+          relative
+          w-full max-w-208.5 h-48.25
+          bg-white
+          rounded-b-[1.5625rem]
+          shadow-[0_0_17.6px_0_rgba(0,0,0,0.25)]
+          z-10
+          flex items-start justify-center pt-5
+        '
+      >
+        <span className='text-[2.8125rem] font-semibold'>연습 중</span>
+
+        <button
+          className='
+            absolute right-6
+            text-[2.1875rem] font-semibold
+            text-white bg-[#CE574C]
+            px-4 py-2 rounded-[0.625rem] cursor-pointer
+          '
+          onClick={() => navigate('/main')}
+        >
+          종료
+        </button>
       </div>
-      <div>
-        <span>{misson}</span>
+
+      <div
+        className='
+          w-full max-w-204 h-20.75
+          bg-[#F2F2F2]
+          flex items-center justify-center
+          rounded-b-[1.5625rem]
+          -mt-[5.8rem]
+          z-10
+        '
+      >
+        <span className='text-[2.1875rem] font-bold text-[#3474D3]'>{misson}</span>
       </div>
     </div>
   )
 }
 
-export default practiceHeader
+export default PracticeHeader
