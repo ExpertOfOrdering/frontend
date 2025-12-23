7b79fddb7ee510688325a75eaf6f37d91bd06609
chootkuri <doyadoya@sookmyung.ac.kr>
2025-12-24 03:52:58 +0900

    Feat(#26):주문 시작 버튼 기본 구조 작성

---
+++ src/components/main/StartBtn.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function StartBtn() {
+  return (
+    <div>
+      <button className='flex justify-center items-center w-109.5 h-31.75 p-2.5 bg-[#58AFFF] rounded-[1.25rem] text-white text-[3.75rem] font-extrabold'>
+        주문 연습 시작
+      </button>
+    </div>
+  )
+}
+
+export default StartBtn
