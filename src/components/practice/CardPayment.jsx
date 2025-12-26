a11a9d4886445798945c1e67e96b08beaacf78e2
chootkuri <doyadoya@sookmyung.ac.kr>
2025-12-26 20:04:24 +0900

    Style(#28):카드 결제 컴포넌트 css 적용

--- src/components/practice/CardPayment.jsx
+++ src/components/practice/CardPayment.jsx
@@ -7,17 +7,24 @@ import BackBtn from './BackBtn'
 function CardPayment() {
   const price = '3000원'
   return (
-    <div>
+    <div className='flex flex-col bg-[#EDEDED] w-198.5 rounded-b-[0.625rem]'>
       <ModalHeader text='결제를 진행해주세요' />
-      <p>
-        총 결제금액 <span>{price}원</span>
+      <p className='text-[2.1875rem] font-medium px-5 py-5'>
+        총 결제금액 <span className='text-[2.1875rem] font-semibold text-[#E97800]'>{price}</span>
       </p>
-      <div>
-        <span>카드를 리더기에 드래그해주세요</span>
-        <img src={CardReader} />
-        <img src={CreditCard} />
+      <div className='flex justify-center'>
+        <div className='flex flex-col w-188 h-121 items-center justify-center gap-[2.33rem] bg-white rounded-[0.625rem]'>
+          <span className='font-semibold text-[2.23088rem] bg-[#F5F5F5]'>
+            카드를 리더기에 드래그해주세요
+          </span>
+          <img src={CardReader} />
+          <img src={CreditCard} />
+        </div>
       </div>
-      <BackBtn />
+
+      <div className='flex px-5 py-5'>
+        <BackBtn />
+      </div>
     </div>
   )
 }
