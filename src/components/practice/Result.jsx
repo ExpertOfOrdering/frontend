365ad24b1d9a00e9ed308e3e67d905efdeafe4f5
chootkuri <doyadoya@sookmyung.ac.kr>
2025-12-26 21:11:46 +0900

    Style(#28):연습하기 결과 컴포넌트 css 적용

--- src/components/practice/Result.jsx
+++ src/components/practice/Result.jsx
@@ -1,22 +1,29 @@
 import React from 'react'
 import OrderResult from '@/assets/OrderResult.png'
+import ViewIncorrect from './ViewIncorrect'
 
 function Result() {
   return (
-    <div>
-      <img src={OrderResult} />
-      <div></div>
-      <div>
-        <span>
-          소요시간<span>02:12</span>
-        </span>
-        <span>
-          주문 정확도<span>100%</span>
-        </span>
-      </div>
-      <div>
-        <button>오답보기</button>
-        <button>초기화면으로</button>
+    <div className='absolute h-screen max-w-208.5 w-full overflow-hidden inset-0 z-50 flex items-center justify-center bg-[rgba(8,8,8,0.84)]'>
+      <div className='flex flex-col items-center justify-center'>
+        <div className='flex pb-[3.31rem]'>
+          <img src={OrderResult} />
+        </div>
+        <div className='flex flex-col w-150.5 h-58 rounded-[0.625rem] bg-white justify-center items-center'>
+          <span className='flex font-semibold text-[2.8125rem] gap-[1.88rem]'>
+            소요시간<span className='font-semibold text-[2.8125rem] text-[#909090]'>02:12</span>
+          </span>
+          <span className='flex font-semibold text-[2.8125rem] gap-[1.88rem]'>
+            주문 정확도<span className='font-semibold text-[2.8125rem] text-[#3474D3]'>100%</span>
+          </span>
+        </div>
+        <div className='flex gap-7 font-semibold text-[2.1875rem] py-5'>
+          <button className='w-71.75 h-20.5 rounded-[0.625rem] bg-[#CCC]'>오답보기</button>
+          <button className='w-71.75 h-20.5 text-[2.1875rem] rounded-[0.625rem] bg-[#CCC]'>
+            초기화면으로
+          </button>
+        </div>
+        <div>{/* <ViewIncorrect /> */}</div>
       </div>
     </div>
   )
--- src/components/practice/ViewIncorrect.jsx
+++ src/components/practice/ViewIncorrect.jsx
@@ -1,21 +1,32 @@
 import React from 'react'
+import X from '@/assets/ResultX.svg'
 
 function ViewIncorrect() {
   return (
-    <div>
-      <div>
-        <span>오답보기</span>
-        <button>X</button>
+    <>
+      <div className='flex flex-col justify-center w-196.5 h-85 rounded-[0.625rem] bg-white'>
+        <div className='flex items-center justify-center'>
+          <div className='flex items-center w-187.25 border-b-2'>
+            <span className='flex justify-center font-semibold text-[2.8125rem] pl-74 pr-60 py-3'>
+              오답보기
+            </span>
+            <button className=''>
+              <img className='w-14 h-14' src={X} />
+            </button>
+          </div>
+        </div>
+        <div className='flex flex-col text-[2.5rem] font-semibold gap-8 px-8 py-8'>
+          <div className='flex items-center gap-5'>
+            <span className='px-6 py-2 bg-[#E9E9E9] rounded-[0.625rem]'>미션</span>
+            <span>뜨거운 캐모마일 1개 포장주문</span>
+          </div>
+          <div className='flex items-center gap-5'>
+            <span className='px-6 py-2 bg-[#F7CCCC] rounded-[0.625rem]'>결과</span>
+            <span>차가운 캐모마일 1개 매장주문</span>
+          </div>
+        </div>
       </div>
-      <div>
-        <span>미션</span>
-        <span>뜨거운 캐모마일 1개 포장주문</span>
-      </div>
-      <div>
-        <span>결과</span>
-        <span>차가운 캐모마일 1개 매장주문</span>
-      </div>
-    </div>
+    </>
   )
 }
 
