16b44ce610290b281da52573763d1589845c2f86
chootkuri <doyadoya@sookmyung.ac.kr>
2025-12-26 15:29:52 +0900

    Feat(#28):결제 모달 기본 UI 컴포넌트 구현

---
+++ src/components/practice/CardPayment.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import ModalHeader from './ModalHeader'
+import CardReader from '@/assets/CardReader.svg'
+import CreditCard from '@/assets/CreditCard.svg'
+import BackBtn from './BackBtn'
+
+function CardPayment() {
+  const price = '3000원'
+  return (
+    <div>
+      <ModalHeader text='결제를 진행해주세요' />
+      <p>
+        총 결제금액 <span>{price}원</span>
+      </p>
+      <div>
+        <span>카드를 리더기에 드래그해주세요</span>
+        <img src={CardReader} />
+        <img src={CreditCard} />
+      </div>
+      <BackBtn />
+    </div>
+  )
+}
+
+export default CardPayment
