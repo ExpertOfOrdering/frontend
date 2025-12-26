49041932128361359e0434569ce3b6d04bfa1a58
yexnsxo <yslove829@naver.com>
2025-12-26 16:37:26 +0900

    Feat(#13): 토큰 대신 유저 아이디로 연습기록 조회하도록 코드 수정

--- src/apis/practiceLog.js
+++ src/apis/practiceLog.js
@@ -1,8 +1,9 @@
-import { api } from '@/apis/auth.js'
+import { api } from '@/apis/auth'
 
 export const getUserLog = async (month) => {
+  const userId = localStorage.getItem('userId')
   const res = await api.get('/api/mypage', {
-    params: { month },
+    params: { month: Number(month), userId: Number(userId) },
   })
   return res.data
 }
