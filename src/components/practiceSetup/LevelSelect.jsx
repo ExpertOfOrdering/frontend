ec74ac602cdd81de3cae94e0c0c0499d9d6ab300
chootkuri <doyadoya@sookmyung.ac.kr>
2025-12-27 13:30:42 +0900

    Feat(#20):난이도 선택 및 조회 기능 추가

--- src/components/practiceSetup/LevelSelect.jsx
+++ src/components/practiceSetup/LevelSelect.jsx
@@ -2,8 +2,22 @@ import React from 'react'
 import PracticeSetupTitle from './PracticeSetupTitle'
 import LevelCard from './LevelCard'
 import { levelData } from './levelData'
+import { useState, useEffect } from 'react'
+import { getCurrentDifficulty } from '@/apis/level'
 
 function LevelSelect({ selectedLevel, onSelect }) {
+  const [recommended, setRecommended] = useState(null)
+
+  useEffect(() => {
+    const userId = localStorage.getItem('userId')
+    if (!userId) return
+
+    getCurrentDifficulty(userId).then((levelText) => {
+      const map = { 초보: 1, 중수: 2, 달인: 3 }
+      setRecommended(map[levelText])
+    })
+  }, [])
+
   return (
     <div className='w-full max-w-208.5 flex flex-col items-center gap-10'>
       <PracticeSetupTitle text='연습 난이도를 선택하세요' />
@@ -15,6 +29,7 @@ function LevelSelect({ selectedLevel, onSelect }) {
             bgColor={level.bgColor}
             textColor={level.textColor}
             isSelected={selectedLevel === level.id}
+            isRecommended={recommended === level.id}
             onClick={() => onSelect(level.id)}
           />
         ))}
