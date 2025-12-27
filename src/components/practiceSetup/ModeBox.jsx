a575493c550dbd5069b2166de82e2a2acd6591b3
chootkuri <doyadoya@sookmyung.ac.kr>
2025-12-27 12:48:40 +0900

    Refactor(#20):카테고리 선택 카페/음료 제외하고 클릭 막기

--- src/components/practiceSetup/ModeBox.jsx
+++ src/components/practiceSetup/ModeBox.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 
-function ModeBox({ img, name, bgColor, textColor, isSelected, onClick }) {
+function ModeBox({ img, name, bgColor, textColor, isSelected, onClick, disabled }) {
   return (
     <button
-      onClick={onClick}
+      onClick={disabled ? undefined : onClick}
+      disabled={disabled}
       className={`
-        flex flex-col w-89.75 h-[17.8175rem] items-center justify-center rounded-[1.5625rem] text-[3.125rem] font-semibold hover:scale-[1.03] cursor-pointer
+        flex flex-col w-89.75 h-[17.8175rem] items-center justify-center rounded-[1.5625rem] text-[3.125rem] font-semibold
+        ${disabled ? 'cursor-not-allowed' : 'hover:scale-[1.03] cursor-pointer'}
         ${bgColor}
         ${textColor}
       `}
